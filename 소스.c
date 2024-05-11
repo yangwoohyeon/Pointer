fac1caab72707668aa24753808fc0bba31bf33f2
yangwoohyeon <dngus0717@gmail.com>
2024-05-11 15:27:42 +0900

    queue made by C

--- 소스.c
+++ 소스.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
-#define SIZE 100
-void enqueue(int n);
-int dequeue();
-void show();
-int queue[SIZE];
-int Rear = -1;
-int Front = -1;
-int main()
-{
-	enqueue(1);
-	enqueue(2);
-	enqueue(3);
-	enqueue(4);
-	enqueue(5);
-	show();
-	dequeue();
-	dequeue();
-	show();
-}
+#include <stdlib.h>
 
-void enqueue(int n) {
-	if (Rear >= SIZE) {
-		printf("오버플로우\n");
-		return;
-	}
-	if (Front == -1) {
-		Front = 0;
-		Rear = Front;
-	}
-	Rear = Rear + 1;
-	queue[Rear] = n;
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct node* front = NULL;
+struct node* rear = NULL;
+
+void enqueue(int value) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+
+    if (new_node == NULL) {
+        printf("메모리 할당 오류\n");
+        exit(1);
+    }
+
+    new_node->data = value;
+    new_node->next = NULL;
+
+    if (rear == NULL) {
+        rear = new_node;
+        front = new_node;
+    }
+    else {
+        rear->next = new_node;
+        rear = new_node;
+    }
 }
 
 int dequeue() {
-	if (Front > Rear) {
-		"언더플로우\n";
-		return;
-	}
-	int value = queue[Front];
-	Front = Front + 1;
-	return value;
+    if (front == NULL) {
+        printf("언더플로우\n");
+        exit(1);
+    }
+
+    struct node* del = front;
+    int value = del->data;
+    front = front->next;
+    free(del);
+
+    if (front == NULL) {
+        rear = NULL;
+    }
+
+    return value;
 }
 
-void show() {
-	for (int i = Front+1; i <= Rear; i++) {
-		printf("%d\n", queue[i]);
-	}
-	printf("\n");
+void display() {
+    struct node* i = front;
+
+    while (i != NULL) {
+        printf("%d\n", i->data);
+        i = i->next;
+    }
+
+    printf("\n");
+}
+
+int main() {
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    enqueue(4);
+    enqueue(5);
+    display();
+
+    dequeue();
+    dequeue();
+    display();
+
+    return 0;
 }
