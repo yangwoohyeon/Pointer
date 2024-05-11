cb3eaa00766e4419503fc0a03ad94567c509146f
yangwoohyeon <dngus0717@gmail.com>
2024-05-11 15:50:56 +0900

    원소의 삭제

--- 소스.c
+++ 소스.c
@@ -1,75 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct node {
-    int data;
-    struct node* next;
-};
+int deleteElement(int arr[],int *size,int value) {
+	bool check = false;
+	int position = 0;
+	for (int i = 0; i < *size; i++) {
+		if (arr[i] == value) {
+			check = true;
+			position = i;
+		}
+	}
+	if (check == false) {
+		return 0;
+	}
+	else {
+		for (int i = position; i < *size; i++) {
+			if (i == *size - 1) {
+				*size=*size - 1;
+				return value;
+			}
+			arr[i] = arr[i + 1];
 
-struct node* front = NULL;
-struct node* rear = NULL;
-
-void enqueue(int value) {
-    struct node* new_node = (struct node*)malloc(sizeof(struct node));
-
-    if (new_node == NULL) {
-        printf("메모리 할당 오류\n");
-        exit(1);
-    }
-
-    new_node->data = value;
-    new_node->next = NULL;
-
-    if (rear == NULL) {
-        rear = new_node;
-        front = new_node;
-    }
-    else {
-        rear->next = new_node;
-        rear = new_node;
-    }
-}
-
-int dequeue() {
-    if (front == NULL) {
-        printf("언더플로우\n");
-        exit(1);
-    }
-
-    struct node* del = front;
-    int value = del->data;
-    front = front->next;
-    free(del);
-
-    if (front == NULL) {
-        rear = NULL;
-    }
-
-    return value;
-}
-
-void display() {
-    struct node* i = front;
-
-    while (i != NULL) {
-        printf("%d\n", i->data);
-        i = i->next;
-    }
-
-    printf("\n");
+		}
+	}
 }
 
 int main() {
-    enqueue(1);
-    enqueue(2);
-    enqueue(3);
-    enqueue(4);
-    enqueue(5);
-    display();
+	int arr[] = { 1, 2, 3, 4, 5 };
+	int size = 5; // 배열의 초기 크기
+	int value, found;
+	
+	scanf_s("%d", &value); //삭제할 값 입력
+	found = deleteElement(arr, &size, value);
+	
+	if (found) {
+		for (int i = 0; i < size; i++) {
+			printf("%d ", arr[i]);
+		}
+	}
 
-    dequeue();
-    dequeue();
-    display();
-
-    return 0;
+	else
+		printf("Not found");
+	
+	return 0;
 }
