ca0a55fa07237f9d53e6c15dfee948e06d048e02
yangwoohyeon <dngus0717@gmail.com>
2024-05-12 13:42:35 +0900

    구조체

--- 소스.c
+++ 소스.c
@@ -1,47 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-#include <stdbool.h>
 
-int deleteElement(int arr[],int *size,int value) {
-	bool check = false;
-	int position = 0;
-	for (int i = 0; i < *size; i++) {
-		if (arr[i] == value) {
-			check = true;
-			position = i;
-		}
-	}
-	if (check == false) {
-		return 0;
-	}
-	else {
-		for (int i = position; i < *size; i++) {
-			if (i == *size - 1) {
-				*size=*size - 1;
-				return value;
-			}
-			arr[i] = arr[i + 1];
+struct Date {
+	int year;
+	int month;
+	int day;
+};
 
-		}
-	}
-}
+struct BirthDay {
+	char* name;
+	struct Date bday;
 
+};
 int main() {
-	int arr[] = { 1, 2, 3, 4, 5 };
-	int size = 5; // 배열의 초기 크기
-	int value, found;
-	
-	scanf_s("%d", &value); //삭제할 값 입력
-	found = deleteElement(arr, &size, value);
-	
-	if (found) {
-		for (int i = 0; i < size; i++) {
-			printf("%d ", arr[i]);
-		}
-	}
+	struct BirthDay bd;
 
-	else
-		printf("Not found");
-	
-	return 0;
-}
+	bd.name = "Gildong";
+	bd.bday.year=2001;
+	bd.bday.month = 7;
+	bd.bday.day = 17;
+	printf("%s\n", bd.name);
+	printf("%d %d %d", bd.bday.year, bd.bday.month, bd.bday.day);
+} 
